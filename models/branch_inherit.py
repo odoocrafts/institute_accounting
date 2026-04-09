17edb7bee76d95b1fb049be16f756ca920267778
muhammadanaswayanad <muhammadanaswayanad@gmail.com>
2026-04-09 23:19:07 +0530

    feat: add accountant signature and contact details to branch model and report templates

--- models/branch_inherit.py
+++ models/branch_inherit.py
@@ -6,3 +6,7 @@ class BranchInherit(models.Model):
     billing_address = fields.Text(string='Billing Address')
     gstin = fields.Char(string='GSTIN')
     accountant_id = fields.Many2one('res.users', string='Branch Accountant')
+    email = fields.Char(string='Email')
+    phone = fields.Char(string='Phone')
+    accountant_signature = fields.Image(string='Accountant Signature', max_width=128, max_height=128)
+
